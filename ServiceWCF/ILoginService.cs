3a868a155b3052e101fc08876eef768655fefb56
César Rodríguez <cesar.rrguez@gmail.com>
2021-01-22 16:50:16 +0100

    Create a new service application project with a login service

---
+++ ServiceWCF/ILoginService.cs
@@ -0,0 +1,22 @@
+﻿using System.Runtime.Serialization;
+using System.ServiceModel;
+
+namespace ServiceWCF
+{
+    [ServiceContract]
+    public interface ILoginService
+    {
+        [OperationContract]
+        bool ValidateLogin(User user);
+    }
+
+    [DataContract]
+    public class User
+    {
+        [DataMember]
+        public string Login { get; set; }
+
+        [DataMember]
+        public string Password { get; set; }
+    }
+}
---
+++ ServiceWCF/LoginService.svc.cs
@@ -0,0 +1,10 @@
+﻿namespace ServiceWCF
+{
+    public class LoginService : ILoginService
+    {
+        public bool ValidateLogin(User user)
+        {
+            return user.Login == "cesar.rrguez" && user.Password == "123456";
+        }
+    }
+}
