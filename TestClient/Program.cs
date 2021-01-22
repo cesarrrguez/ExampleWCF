86d2fee2b28307930b677b0f24df0cf6af391bc2
César Rodríguez <cesar.rrguez@gmail.com>
2021-01-22 17:00:52 +0100

    Create a new console project to login service test

---
+++ TestClient/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using ServiceReference;
+
+namespace TestClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = new LoginServiceClient();
+
+            Console.WriteLine("Enter login:");
+            var login = Console.ReadLine();
+
+            Console.WriteLine("Enter password:");
+            var password = Console.ReadLine();
+
+            var user = new User
+            {
+                Login = login,
+                Password = password
+            };
+
+            var result = client.ValidateLoginAsync(user).Result;
+            Console.WriteLine(result ? "Successful connection" : "Failed connection");
+        }
+    }
+}
